567c4143607227313dd820c11c4712abd9eda2f5
Greg Niemann <greg.niemann@gmail.com>
2015-11-30 22:29:25 -0600

    tsp1.py now works for all 3 examples

--- tsp1.py
+++ tsp1.py
@@ -3,30 +3,59 @@
 from __future__ import print_function
 import math
 import optparse
+import random
+
+
+class Point:
+	'''This is a simple class which defines a point in 2D space
+	It has two member functions which determine distance between two points'''
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+	
+	def getRelativeDistance(self, p2):
+		'''Gets the relative distance between two points, useful only for comparing distances'''
+		return pow(p2.x - self.x, 2) + pow(p2.y - self.y, 2)
+		
+	def getDistance(self, p2):
+		'''Gets the actual distance between two points'''
+		return round(math.sqrt(self.getRelativeDistance(p2)))
 
 def loadCities(filename):
 	input_file = open(filename, "r")
-	cities = [map(int, line.split()) for line in input_file]
+	cities = []
+	for line in input_file:
+		city, x, y = map(int, line.split())
+		cities.append(Point(x, y))
+		
 	input_file.close()
 	
 	return cities
 	
-def getDistance(p1, p2):
-	return round(math.sqrt(pow((p2[0] - p1[0]), 2) + pow((p2[1] - p1[1]), 2)))
+
+def getNextCity(curCity, cities, remainingCities):
+	'''
+	Iterate through the remaining cities, determine the relative distance between each
+	And the current city. Select the smallest relative distance and return that city
+	and the actual distance between the two
+	'''
 	
-def getDistanceMatrix(cities):
-	allDistances = {}
-	for (city, x, y) in cities:
-		distances = []
-		for (otherCity, x2, y2) in cities:
-			if city != otherCity:
-				distances.append((getDistance((x, y), (x2, y2)), otherCity))
-		distances.sort()
-		allDistances[city] = distances
-	
-	return allDistances
-	
-def nearestNeighborTSP(cities, distances, startingCity):
+	minRelDistance = float('inf')
+	closestCity = -1
+	for otherCity in remainingCities:
+		relDistance = cities[curCity].getRelativeDistance(cities[otherCity])
+		if relDistance < minRelDistance:
+			minRelDistance = relDistance
+			closestCity = otherCity
+			
+	return (closestCity, cities[curCity].getDistance(cities[closestCity]))
+		
+def nearestNeighborTSP(cities, startingCity):
+	'''
+	Uses the Nearest Neighbor heuristic to find a path through all the cities,
+	starting from the startingCity. Returns a 2-tuple of the length of the path
+	and the path taken
+	'''
 	pathLength = 0
 	path = []
 	
@@ -38,33 +67,38 @@ def nearestNeighborTSP(cities, distances, startingCity):
 	curCity = startingCity
 	
 	while len(remainingCities) > 0:
-		# find closest un-used city
-		i = 0
-		while distances[curCity][i][1] not in remainingCities:
-			i += 1
-		
-		closestNeighbor = distances[curCity][i][1]
-		minDistance = distances[curCity][i][0]
-				
-		path.append(closestNeighbor)
-		pathLength += minDistance
+		(closestNeighbor, distance) = getNextCity(curCity, cities, remainingCities)
+
+		pathLength += distance
 		remainingCities.remove(closestNeighbor)
+		path.append(closestNeighbor)
 		curCity = closestNeighbor
 		
 	return (int(pathLength), path)
 	
-def repativeNearestNeighborTSP(cities, distances):
+def repeatitiveNearestNeighborTSP(cities):
+	'''
+	Runs the nearest neighbor algorithm on cities, starting from each city
+	Selects the path (of the N tried) with the shortest path
+	'''
 	shortestPath = []
 	shortestPathLength = float('inf')
 	
 	for startingCity in range(len(cities)):
-		(distance, path) = nearestNeighborTSP(cities, distances, startingCity)
+		(distance, path) = nearestNeighborTSP(cities, startingCity)
 		if (distance < shortestPathLength):
 			shortestPath = path
 			shortestPathLength = distance
 			
 	return (shortestPathLength, shortestPath)
 	
+def randomNearestNeighbor(cities):
+	'''
+	Runs the nearest neighbor on cities, starting from a random city
+	'''
+	startingCity = random.randint(0, len(cities))
+	return nearestNeighborTSP(cities, startingCity)
+
 if __name__ == "__main__":
 	p = optparse.OptionParser()
 	
@@ -72,11 +106,11 @@ if __name__ == "__main__":
 	
 	cities = loadCities(args[0])
 	
-	distances = getDistanceMatrix(cities)
-	
-	(distance, path) = repativeNearestNeighborTSP(cities, distances)
+	random.seed()
+		
+	(distance, path) = randomNearestNeighbor(cities)
 
 	output_file = open(args[0] + ".path", "w")
-	print(distance)
+	print(distance, file=output_file)
 	for city in path:
-		print(city)
+		print(city, file=output_file)
